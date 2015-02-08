5c410abc5b3a7addfd1e7ab5dbb68fe522032459
Khairil Yusof <khairil.yusof@gmail.com>
2015-02-08 12:31:50 +0800

    Add initial subregion vocabulary based on http://unstats.un.org/unsd/methods/m49/m49regin.htm

---
+++ ploneun/vocabulary/vocabulary/subregion.py
@@ -0,0 +1,36 @@
+from five import grok
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+from zope.schema.interfaces import IVocabularyFactory
+from zope.component import getUtility
+from z3c.formwidget.query.interfaces import IQuerySource
+
+class UNGeographicalSubRegion(grok.GlobalUtility):
+
+    # UN Data Codes and Categorization
+    # http://unstats.un.org/unsd/methods/m49/m49regin.htm
+
+    grok.name('ploneun.vocabulary.subregion')
+    grok.implements(IVocabularyFactory)
+
+    #FIXME incomplete, and should be from lookup csv file
+    _terms = [
+        {
+        'value': '014',
+        'title': 'Easter Africa',
+        },
+        {
+        'value': '017',
+        'title': 'Eastern Africa',
+        },
+        {
+        'value': '029',
+        'title': 'Caribbean',
+        },
+        ]
+
+    def __call__(self, context):
+        terms = []
+        for i in self._terms:
+            terms.append(SimpleTerm(**i))
+        return SimpleVocabulary(terms)
